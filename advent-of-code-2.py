86862d889bb931d3668f972f6f123cdc7aa5d9cf
Caitlin Wright <caitlinwright@Caitlins-MacBook-Pro.local>
2021-02-14 20:38:21 -0800

    Day 2 Part 2 solution added.

--- advent-of-code-2.py
+++ advent-of-code-2.py
@@ -20,3 +20,26 @@ def password_check(input):
 
 print(password_check("input.txt"))
 
+def password_check_part2(input):
+    password_count = 0
+    with open('input.txt') as f:
+        for line in f:
+            password = line.split(" ", 2)[2]
+            letter = line.split(":", 1)[0][-1]
+            policy = line.split(" ", 1)[0]
+            first_num = int(policy.split("-")[0])
+            second_num = int(policy.split("-")[1])
+
+            count = 1
+            valid_password = 0
+            for character in password:
+                if (count == first_num and character == letter) or (count == second_num and character == letter):
+                    valid_password += 1
+                count += 1
+            
+            if valid_password == 1:
+                password_count += 1
+    
+    return password_count
+
+print(password_check_part2("input.txt"))
